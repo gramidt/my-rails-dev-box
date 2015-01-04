2fd2112d9e22b224c7c9b7d4c7675218697618da
Granville Schmidt <granville.schmidt@yahoo.com>
2015-01-04 11:13:04 -0500

    fixed RVM curl bug caused by new key requirement, and added Ruby 2.2.0

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -111,20 +111,20 @@ package { 'nodejs':
 # --- Ruby ---------------------------------------------------------------------
 
 exec { 'install_rvm':
-  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  command => "${as_vagrant} 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && curl -sSL https://get.rvm.io | bash -s stable'",
   creates => "${home}/.rvm",
   require => Package['curl']
 }
 
 exec { 'install_ruby':
-  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --autolibs=enabled && ${home}/.rvm/bin/rvm install 2.1.0 --autolibs=enabled'",
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install 2.0.0 --autolibs=enabled && ${home}/.rvm/bin/rvm install 2.1.0 --autolibs=enabled && ${home}/.rvm/bin/rvm install 2.2.0 --autolibs=enabled'",
   creates => "${home}/.rvm/bin/ruby",
   timeout => 600,
   require => [ Package['libyaml-dev'], Exec['install_rvm'] ]
 }
 
-exec { 'set_default_ruby': 
-  command => "${as_vagrant} '${home}/.rvm/bin/rvm --fuzzy alias create default 2.1.0 && ${home}/.rvm/bin/rvm use default'",
+exec { 'set_default_ruby':
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm --fuzzy alias create default 2.2.0 && ${home}/.rvm/bin/rvm use default'",
   require => Exec['install_ruby']
 }
 
